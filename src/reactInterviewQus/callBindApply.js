95988fce9a483ebddd12ffab00fc058e9a2b0f79
pramodlohra <pramod.lohra@nathcorp.com>
2024-11-09 20:09:57 +0530

    reached Queue of YT Ques

--- src/reactInterviewQus/callBindApply.js
+++ src/reactInterviewQus/callBindApply.js
@@ -1,19 +1,32 @@
-const name1 = {
-    firstName: "pramod",
-    lastName: "lohra",
-    printFullName: function () {
-        console.log(`Full name is: ${this.firstName} ${this.lastName}`)
-    }
+// const name1 = {
+//     firstName: "pramod",
+//     lastName: "lohra",
+//     printFullName: function () {
+//         console.log(`Full name is: ${this.firstName} ${this.lastName}`)
+//     }
+
+// }
+
+
+// const name2 = {
+//     firstName: "keshav",
+//     lastName: "kumar",
+
+
+// }
+
+// name1.printFullName();
+// name1.printFullName.call(name2);
+
+
+const name1={
+    firstName:"Pramod",
+    lastName:"Lohra",
 
 }
 
-
-const name2 = {
-    firstName: "keshav",
-    lastName: "kumar",
-
-
+let printFullName=function(){
+    console.log(`${this.firstName} ${this.lastName}`);
 }
 
-name1.printFullName();
-name1.printFullName.call(name2);
+printFullName.call(name1);
